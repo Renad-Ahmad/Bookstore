824a8727e0b94dc7e024f0267196a47856cc032d
Renad-Ahmad <rena.34end@gmail.com>
2021-07-12 18:30:32 +0300

    2d array

---
+++ main.js
@@ -0,0 +1,8 @@
+let books = [
+   [["id",1],["Book Title","Start with why"],["Author","Simon Sinek"],["Price",80.0],["Quantity",13]] ,
+   [["id",2],["Book Title","But how do it know"],["Author","J. Clark Scott"],["Price",59.9],["Quantity",22]] ,
+   [["id",3],["Book Title","Clean Code"],["Author","Robert Cecil Martin"],["Price",50.0],["Quantity",5]] ,
+   [["id",4],["Book Title","Zero to One"],["Author","Peter Thiel"],["Price",45.0],["Quantity",12]] ,
+   [["id",5],["Book Title","You don't know JS"],["Author","Kyle Simpson"],["Price",39.9],["Quantity",9]] 
+]
+console.log(books);
